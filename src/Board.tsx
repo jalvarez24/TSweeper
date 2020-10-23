f00ac6c2ffdd8f9fd64557c43e25a6b001767f7b
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2020-10-22 22:00:12 -0700

    initial post after removing most boiler plate

---
+++ src/Board.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Board: React.FC<Props> = () => {
+    return (
+        <div>
+            Board
+        </div>
+    )
+}
+
+export default Board
